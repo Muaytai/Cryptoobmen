8eb5a61a0987fc596c21048c710c370b68eaa70a
Makc <muay-tai@mail.ru>
2025-04-28 19:12:33 +0300

    Добавлено отключение требования слэша в конце URL в настройках Django, обновлены конфигурации маршрутов в Next.js для обработки запросов с завершающим слэшем. Переработаны страницы входа и регистрации, вынесены формы в отдельные компоненты, улучшен интерфейс и обработка ошибок. Обновлены стили для страницы регистрации.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8000/api/:path*/', // Добавляем слэш в конце каждого запроса
+      },
+      {
+        source: '/api/:path*/',
+        destination: 'http://localhost:8000/api/:path*/', // Явно обрабатываем URL с завершающим слэшем
+      },
+    ];
+  },
 };
 
 export default nextConfig;
