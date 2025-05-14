d9ad44a275d087060a7d1594f0b2421d7a7de978
Mr. EnglishCat <mr.englishcat@gmail.com>
2025-05-14 10:49:13 +0300

    отредактировал checkbox в цвета макета

--- frontend/src/app/register/RegisterForm.tsx
+++ frontend/src/app/register/RegisterForm.tsx
@@ -7,6 +7,7 @@ import {useAuthStore} from '@/store/useAuthStore';
 import {FaEye, FaEyeSlash, FaGoogle, FaApple} from 'react-icons/fa';
 import styles from './Register.module.css';
 import {Input} from "@/components/ui/Input";
+import InputCheckbox from "@/components/modalWindows/check forms/InputCheckbox";
 
 export default function RegisterForm() {
     const router = useRouter();
@@ -156,13 +157,20 @@ export default function RegisterForm() {
                                 </button>
                             </div>
                             <div className={styles.checkboxRow}>
-                                <input
-                                    type="checkbox"
-                                    name="agree"
-                                    checked={formData.agree}
-                                    onChange={handleChange}
-                                    className={styles.checkbox}
-                                    required
+                                {/*<input*/}
+                                {/*    type="checkbox"*/}
+                                {/*    name="agree"*/}
+                                {/*    checked={formData.agree}*/}
+                                {/*    onChange={handleChange}*/}
+                                {/*    className={styles.checkbox}*/}
+                                {/*    required*/}
+                                {/*/>*/}
+                                <InputCheckbox
+                                    idInput={"agreepment"}
+                                    nameInput={"agreepment"}
+                                    valueInput={"service"}
+                                    // label={"Услуга"}
+                                    // typeInput={""}
                                 />
                                 <span className={styles.agreeText}>
               Я соглашаюсь с <Link href="/terms" className={styles.link}>Условиями использования</Link> и <Link
---
+++ frontend/src/components/modalWindows/check forms/InputCheckbox.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./InputCheckbox.module.css"
+
+
+const InputCheckbox = ({idInput, nameInput, valueInput, label = "", typeInput = "checkbox"}) => {
+    return (
+        <div className={styles.checkBoxWrapper}>
+            <input
+                type={typeInput}
+                id={idInput}
+                name={nameInput}
+                value={valueInput}
+                className={styles.checkbox}
+            />
+            <label
+                className={styles.label}
+                htmlFor={idInput}>
+                {label}
+            </label>
+        </div>
+    );
+};
+
+
+export default InputCheckbox;
