9a37d2054c14e5e28902407a35d6d51397617151
Dmitry Shishkov <dim457676@gmail.com>
2025-11-10 20:10:09 +0300

    Cleanup: remove obsolete docs and backups

--- backend/tmp_check_memos.py
+++
@@ -1,7 +0,0 @@
-import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-import django
-django.setup()
-from crypto.models import UserDepositMemo
-qs = UserDepositMemo.objects.filter(currency__symbol='XRP', status='waiting')
-print(list(qs.values('memo','created_at')))
